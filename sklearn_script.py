61431224184316e721f85bf6e7ba33d73e7c12be
KzKe <30785608+KzKe@users.noreply.github.com>
2020-09-16 15:04:57 +0800

    添加了需要加入代码的包

---
+++ sklearn_script.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :sklearn_script.py
+# @Time      :2020/9/16 2:53 下午
+# @Author    :Kangke
+
+import sklearn
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
---
+++ tensorflow_script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :tensorflow_script.py
+# @Time      :2020/9/16 2:58 下午
+# @Author    :Kangke
+
+
+if __name__ == "__main__":
+    run_code = 0
