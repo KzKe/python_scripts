c7675b98976f1eff7d3471dbbf8f52c0525fa24c
KzKe <30785608+KzKe@users.noreply.github.com>
2020-09-17 10:15:41 +0800

    更新目录

---
+++ algorithms_script/fasttext.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :fasttext.py.py
+# @Time      :2020/9/17 10:09 上午
+# @Author    :Kangke
+
+
+import tensorflow
+import fasttext
+import torch
---
+++ packages_script/open_script.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :open_script.py
+# @Time      :2020/9/17 8:55 上午
+# @Author    :Kangke
+
+
+# with
