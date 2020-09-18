970c83e45dc66325b686f6639ed6635681cd5d65
KzKe <30785608+KzKe@users.noreply.github.com>
2020-09-18 09:29:21 +0800

    删除了torch文本分类的相关代码，单独建立了项目

--- algorithms_script/torch_text_classfication/predict.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# @FileName  :predict.py.py
-# @Time      :2020/9/18 8:40 上午
-# @Author    :Kangke
-
-
-if __name__ == "__main__":
-    run_code = 0
--- algorithms_script/torch_text_classfication/train.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# @FileName  :train.py.py
-# @Time      :2020/9/18 8:41 上午
-# @Author    :Kangke
-
-
-if __name__ == "__main__":
-    run_code = 0
