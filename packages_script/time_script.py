90deb07ba8160637ffcd68fdcd66d7e673c51ce6
KzKe <30785608+KzKe@users.noreply.github.com>
2020-10-11 17:09:24 +0800

    添加了时间模块

---
+++ packages_script/time_script.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: KeKang
+@contact:***@**.com
+@version: 1.0.0
+@license: Apache Licence
+@file: time_script.py
+@time: 2020/10/11 5:03 下午
+
+使用 time 进行计时
+"""
+
+
+import time
+
+t0 = time.time()
+# 操作
+t1 = time.time()
+print(t1 - t0)
